fbb026eeb2011979452a44290008520f73756977
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-04-11 17:59:02 -0400

    Agregamos los seeders de Users, profile, location, image, group

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(App\Group::class, 3)->create();
+
+        factory(App\Level::class)->create(['name'=>'oro']);
+        factory(App\Level::class)->create(['name'=>'plata']);
+        factory(App\Level::class)->create(['name'=>'bronce']);
+
+        factory(App\User::class, 5)->create()->each( function ($user){
+            $profile = $user->profile()->save(factory(App\Profile::class)->make());
+
+            $profile = location()->save(factory(App\Location::class)->make());
+
+            $user = groups()->attach($this->array(rand(1,3)));
+
+            $user = image()->save(factory(App\Image::class)->make([
+                'url' => 'https://lorempixel.com/90/90/'
+            ]));
+        });
+    }
+
+    public function array($max){
+        $values = [];
+        for ($i=1; $i < $max; $i++) { 
+            $values[] = $i
+        }
+        return $values;
     }
 }
