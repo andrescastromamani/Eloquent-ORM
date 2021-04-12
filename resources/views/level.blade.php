a63c2c9efded1b82dba4c5b104e2ad47ed188e88
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-04-11 22:39:32 -0400

    Agregamos la la vista de niveles

--- resources/views/level.blade.php
+++ resources/views/level.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ $user->name }}</title>
+    <title>usuario del {{ $level->name }}</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -16,35 +16,11 @@
     <div class="container shadow">
         <div class="row">
             <div class="col mt-3 shadow">
-                <img src="{{ $user->image->url }}" alt="image" class="float-left rounded-circle">
-                <h1>{{ $user->name }}</h1>
-                <span>{{ $user->email }}</span> <br>
-                <strong>Instagram: </strong>{{ $user->profile->instagram }} <br>
-                <strong>github: </strong>{{ $user->profile->github }} <br>
-                <strong>web: </strong>{{ $user->profile->web }} <br>
-                <p>
-                    <strong>Pais: </strong>{{ $user->location->country }} <br>
-                    <strong>Nivel:
-                        @if ($user->level)
-                            <a href="{{route('level', $user->level->id)}}">{{ $user->level->name }}</a>
-                        @else
-                            ---
-                        @endif
-                    </strong>
-                </p>
-                <hr>
-                <p>
-                    <strong>grupo:</strong>
-                    @forelse ($user->groups as $group)
-                    <span class="bg-primary p-1">{{ $group->name }}</span>
-                @empty
-                    <em>No tiene grupo</em>
-                @endforelse
-                </p>
+                <h1>nivel: {{ $level->name }}</h1>
             </div>
         </div>
 
-        <h2 class="color-white">Post</h2>
+        <h2 class="color-white">post de {{$level->name}}</h2>
         <div class="row">
             @foreach ($posts as $post)
             <div class="col-6">
